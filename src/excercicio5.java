011af113890d2cab3bb9347a6196639f2be6bede
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-19 20:45:38 -0300

    excercicio 5 finalizado

---
+++ src/excercicio5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class excercicio5 {
+
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double salarioMinimo;
+    int qtdkwatt;
+    double ValorKwatt, conta;
+
+    System.out.print("Informe o valor do salário mínimo R$ ");
+    salarioMinimo = sc.nextDouble();
+    System.out.print("Informe a quantidade de quilowatt ");
+    qtdkwatt = sc.nextInt();
+
+    ValorKwatt = salarioMinimo / 7 / 100;
+
+    conta = qtdkwatt * ValorKwatt;
+
+    System.out.println("Valor da conta R$ " + conta);
+    System.out.println("Valor do quilowatt R$ " + ValorKwatt);
+    System.out.println("Valor da conta com 10% de desconto R$ " + conta * 0.90);
+
+
+
+
+    }
+}
